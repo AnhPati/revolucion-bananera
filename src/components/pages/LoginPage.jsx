1027dcd29b7c2ee46deb5017a1cc225db73c03f2
Anhpati <romain-felicianne@hotmail.fr>
2023-11-28 19:57:58 +0100

    ✨ F01: LOGIN > Add title, add form

--- src/components/pages/LoginPage.jsx
+++ src/components/pages/LoginPage.jsx
@@ -1,7 +1,36 @@
+import { useState } from "react"
+
 const LoginPage = () => {
+    // state
+    const [inputValue, setInputValue] = useState('')
+
+    // comportements
+    const handleChange = (e) => {
+        setInputValue(e.target.value)
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        alert(`Bonjour ${inputValue}`)
+        setInputValue('')
+    }
+
+    // render
     return (
         <div>
-            LoginPage
+            <h1>Bienvenue chez nous !</h1>
+            <br />
+            <h2>Connectez-vous :</h2>
+            <form action="submit" onSubmit={handleSubmit}>
+                <input
+                    value={inputValue}
+                    type="text"
+                    placeholder="Entrer votre prénom :"
+                    onChange={handleChange}
+                    required
+                />
+                <button>Accéder à votre espace</button>
+            </form>
         </div>
     )
 }
