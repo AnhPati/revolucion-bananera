990416be803a79c1c27d48b8ae6b3539977150e0
Anhpati <romain-felicianne@hotmail.fr>
2023-11-29 00:45:20 +0100

    ✨ F02: ORDER > Add userName to LoginForm recovery

--- src/components/pages/OrderPage.jsx
+++ src/components/pages/OrderPage.jsx
@@ -1,7 +1,12 @@
+import { useLocation } from "react-router-dom"
+
 const OrderPage = () => {
+    const location = useLocation()
+    const userName = location.state.userName
+
     return (
         <div>
-            <h1>Bonjour </h1>
+            <h1>Bonjour {userName}</h1>
             <br />
             <button>Déconnexion</button>
         </div>
