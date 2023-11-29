d759feba0f633dbd6e9fa257f650f0f857f950dd
Anhpati <romain-felicianne@hotmail.fr>
2023-11-30 00:09:51 +0100

    💄 F03: LOGIN > Add LoginForm input's styles, add LoginForm button's styles

--- src/components/pages/login/LoginForm.jsx
+++ src/components/pages/login/LoginForm.jsx
@@ -43,7 +43,8 @@ const LoginFormStyled = styled.form`
     flex-direction: column;
     justify-content: center;
     align-items: center;
-    color: #FFF;
+    color: ${theme.colors.white};
+    font-size: ${theme.fonts.P0};
 
     h1 {
         font-size: ${theme.fonts.P5};
@@ -53,5 +54,49 @@ const LoginFormStyled = styled.form`
     h2 {
         font-size: ${theme.fonts.P4};
         font-weight: ${theme.weights.bold};
+        margin: ${theme.spacing.sm}
     }
-`;
+
+    input,
+    button {
+        width: 400px;
+        border-radius: ${theme.borderRadius.round};
+        padding-top: 18px;
+        padding-bottom: 18px;
+    }
+
+    input {
+        color: ${theme.colors.greyUltraLight};
+        font-weight: ${theme.weights.regular};
+        padding-left: 24px;
+        padding-right: 24px;
+        margin: ${theme.spacing.sm};
+        border: none;
+
+        &:focus-visible {
+            outline: none;
+
+            &::placeholder {
+                color: transparent
+            }
+        }
+    }
+
+    button {
+        color: ${theme.colors.white};
+        font-weight: ${theme.weights.bold};
+        background: ${theme.colors.primary_burger};
+        border: 1px solid ${theme.colors.primary_burger};
+        cursor: pointer;
+
+        &:hover {
+            color: ${theme.colors.primary_burger};
+            background: ${theme.colors.white};
+        }
+
+        &:focus {
+            color: ${theme.colors.white};
+            background: ${theme.colors.primary_burger};
+        }
+    }
+`
