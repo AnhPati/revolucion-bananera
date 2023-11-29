7cfd0f2ab26ff82d8df93b6572d0ab4bcb306882
Anhpati <romain-felicianne@hotmail.fr>
2023-11-29 22:42:35 +0100

    💄 F03: LOGIN > Add LoginPage styles

--- src/components/pages/login/LoginPage.jsx
+++ src/components/pages/login/LoginPage.jsx
@@ -1,9 +1,24 @@
+import styled from "styled-components";
 import { LoginForm } from "./LoginForm"
+import background from "./../../../assets/burger-background.jpg"
 
 const LoginPage = () => {
     return (
-        <LoginForm />
+        <LoginPageStyled>
+            <LoginForm />
+        </LoginPageStyled>
     )
 }
 
 export default LoginPage
+
+const LoginPageStyled = styled.div`
+    min-height: 100vh;
+    background-image: linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url(${background});
+    background-size: cover;
+    background-position: center center;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+`;
