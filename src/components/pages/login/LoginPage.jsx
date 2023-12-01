8b93e3e2aa348ef2b83893fa6d80e98a544ea24e
Anhpati <romain-felicianne@hotmail.fr>
2023-12-01 16:15:28 +0100

    🚚 F03: Rename utils folder to ui folders for reusable components

--- src/components/pages/login/LoginPage.jsx
+++ src/components/pages/login/LoginPage.jsx
@@ -1,7 +1,7 @@
 import styled from "styled-components";
 import { LoginForm } from "./LoginForm"
 import background from "./../../../assets/burger-background.jpg"
-import { Logo } from "../../utils/Logo";
+import { Logo } from "../../ui/Logo";
 
 const LoginPage = () => {
     return (
