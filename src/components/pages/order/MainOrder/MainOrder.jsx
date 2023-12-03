f79d02fef3db46e4c667d399e5f2947724af5ef7
Anhpati <romain-felicianne@hotmail.fr>
2023-12-03 23:22:27 +0100

    💄 F05: 0RDER > MenuOrder layout styles

--- src/components/pages/order/MainOrder/MainOrder.jsx
+++ src/components/pages/order/MainOrder/MainOrder.jsx
@@ -14,5 +14,6 @@ const MainOrderStyled = styled.main`
     flex: 1;
     display: flex;
     border-radius: 0 0 ${theme.borderRadius.extraRound} ${theme.borderRadius.extraRound};
-    box-shadow: 0px 8px 20px 8px rgba(0, 0, 0, 0.20) inset;
+    box-shadow: 0px 8px 20px 8px rgba(0, 0, 0, 0.20) inset;    
+    overflow-y: scroll;
 `;
--- src/components/pages/order/MainOrder/MenuOrder.jsx
+++ src/components/pages/order/MainOrder/MenuOrder.jsx
@@ -1,13 +1,38 @@
 import styled from "styled-components";
+import { useState } from "react";
+import { fakeMenu2 } from '../../../../fakeData/fakeMenu'
 
 export const MenuOrder = () => {
+    const [products, setProducts] = useState(fakeMenu2)
+
     return (
         <MenuOrderStyled>
-            MenuOrder
+            <ul>
+                {products.map(product => {
+                    return (
+                        <li key={product.id} className={'card'}>
+                            <h3>{product.title}</h3>
+                        </li>
+                    )
+                })}
+            </ul>
         </MenuOrderStyled>
     )
 }
 
 const MenuOrderStyled = styled.section`
     flex: 1;
+    padding-top: 50px;
+
+    ul {
+        display: grid;
+        grid-template-columns: 1fr 1fr 1fr 1fr;
+        gap: 85px 60px;
+        list-style: none;
+    }
+
+    .card {        
+        height: 330px;
+        width: 240px;
+    }
 `;
