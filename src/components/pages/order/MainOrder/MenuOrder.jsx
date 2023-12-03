5a8bbb151a0ec87de6849f39c2bf0d7594891d4f
Anhpati <romain-felicianne@hotmail.fr>
2023-12-03 23:54:30 +0100

    💄 F05: ORDER > Add MenuOrder styles card

--- src/components/pages/order/MainOrder/MenuOrder.jsx
+++ src/components/pages/order/MainOrder/MenuOrder.jsx
@@ -1,6 +1,8 @@
 import styled from "styled-components";
 import { useState } from "react";
 import { fakeMenu2 } from '../../../../fakeData/fakeMenu'
+import { PrimaryButton } from '../../../ui/PrimaryButton'
+import { theme } from "../../../../theme";
 
 export const MenuOrder = () => {
     const [products, setProducts] = useState(fakeMenu2)
@@ -11,7 +13,18 @@ export const MenuOrder = () => {
                 {products.map(product => {
                     return (
                         <li key={product.id} className={'card'}>
-                            <h3>{product.title}</h3>
+                            <div className={'card-img'}>
+                                <img src={product.imageSource} alt={product.title} />
+                            </div>
+                            <h3 className={'card-title'}>{product.title}</h3>
+                            <div className={'card-description'}>
+                                <p>
+                                    {product.price} €
+                                </p>
+                                <div className={'add_button-container'}>
+                                    <PrimaryButton label={'Ajouter'} />
+                                </div>
+                            </div>
                         </li>
                     )
                 })}
@@ -31,8 +44,60 @@ const MenuOrderStyled = styled.section`
         list-style: none;
     }
 
-    .card {        
+    .card {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
         height: 330px;
         width: 240px;
+        background: ${theme.colors.white};
+        padding: 50px 20px 10px 20px;
+        box-shadow: -8px 8px 20px 0px rgba(0, 0, 0, 0.20);
+        border-radius: ${theme.borderRadius.extraRound};
+
+        .card-img {
+            height: 145px;
+            width: 200px;
+            text-align: center;
+
+            img {
+                max-width: 100%;
+                max-height: 100%;
+            }
+        }
+
+        .card-title {
+            width: 100%;
+            margin: 15px 0 0 0;
+            color: #17161A;
+            font-size: 36px;
+            font-weight: 700;
+            text-align: start;
+        }
+
+        .card-description {
+            width: 100%;
+            display: grid;
+            grid-template-columns: 1fr 1fr;
+            align-items: center;
+            margin-top: 7.5px;
+
+            p {
+                margin: 0;
+                color: #FFA01B;
+                font-size: 16px;
+                font-weight: 400;
+                line-height: 22px;
+            }
+
+            .add_button-container {
+                width: 95px;
+
+                button {
+                    padding-top: ${theme.gridUnit * 1.5}px;
+                    padding-bottom: ${theme.gridUnit * 1.5}px;
+                }
+            }
+        }
     }
 `;
