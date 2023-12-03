7b75e62050900f59607e0a9271ccfbebc7a87395
Anhpati <romain-felicianne@hotmail.fr>
2023-12-04 00:18:17 +0100

    ♻️ F05: ORDER > Replace MenuOrder li item by Card

--- src/components/pages/order/MainOrder/Card.jsx
+++ src/components/pages/order/MainOrder/Card.jsx
@@ -1,5 +1,82 @@
-export const Card = () => {
+import { PrimaryButton } from '../../../ui/PrimaryButton'
+import { formatPrice } from "../../../../utils/maths";
+import styled from 'styled-components';
+import { theme } from '../../../../theme';
+export const Card = ({ product }) => {
     return (
-        <div>Card</div>
+        <CardStyled key={product.id} className={'card'}>
+            <div className={'card-img'}>
+                <img src={product.imageSource} alt={product.title} />
+            </div>
+            <h3 className={'card-title'}>{product.title}</h3>
+            <div className={'card-description'}>
+                <p>
+                    {formatPrice(product.price)}
+                </p>
+                <div className={'add_button-container'}>
+                    <PrimaryButton label={'Ajouter'} />
+                </div>
+            </div>
+        </CardStyled>
     )
 }
+
+const CardStyled = styled.li`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    height: 330px;
+    width: 240px;
+    background: ${theme.colors.white};
+    padding: 50px 20px 10px 20px;
+    box-shadow: -8px 8px 20px 0px rgba(0, 0, 0, 0.20);
+    border-radius: ${theme.borderRadius.extraRound};
+
+    .card-img {
+        height: 145px;
+        width: 200px;
+        text-align: center;
+
+        img {
+            max-width: 100%;
+            max-height: 100%;
+        }
+    }
+
+    .card-title {
+        width: 100%;
+        overflow: hidden;
+        white-space: nowrap;
+        text-overflow: ellipsis;
+        margin: 15px 0 0 0;
+        color: #17161A;
+        font-size: 36px;
+        font-weight: 700;
+        text-align: start;
+    }
+
+    .card-description {
+        width: 100%;
+        display: grid;
+        grid-template-columns: 1fr 1fr;
+        align-items: center;
+        margin-top: 7.5px;
+
+        p {
+            margin: 0;
+            color: #FFA01B;
+            font-size: 16px;
+            font-weight: 400;
+            line-height: 22px;
+        }
+
+        .add_button-container {
+            width: 95px;
+
+            button {
+                padding-top: ${theme.gridUnit * 1.5}px;
+                padding-bottom: ${theme.gridUnit * 1.5}px;
+            }
+        }
+    }
+`;
--- src/components/pages/order/MainOrder/MenuOrder.jsx
+++ src/components/pages/order/MainOrder/MenuOrder.jsx
@@ -1,9 +1,7 @@
 import styled from "styled-components";
 import { useState } from "react";
 import { fakeMenu2 } from '../../../../fakeData/fakeMenu'
-import { PrimaryButton } from '../../../ui/PrimaryButton'
 import { theme } from "../../../../theme";
-import { formatPrice } from "../../../../utils/maths";
 import { Card } from "./Card";
 
 export const MenuOrder = () => {
@@ -14,21 +12,8 @@ export const MenuOrder = () => {
             <ul>
                 {products.map(product => {
                     return (
-                        <><Card />
-                            <li key={product.id} className={'card'}>
-                                <div className={'card-img'}>
-                                    <img src={product.imageSource} alt={product.title} />
-                                </div>
-                                <h3 className={'card-title'}>{product.title}</h3>
-                                <div className={'card-description'}>
-                                    <p>
-                                        {formatPrice(product.price)}
-                                    </p>
-                                    <div className={'add_button-container'}>
-                                        <PrimaryButton label={'Ajouter'} />
-                                    </div>
-                                </div>
-                            </li></>
+                        <Card key={product.id}
+                            product={product} />
                     )
                 })}
             </ul>
@@ -45,65 +30,5 @@ const MenuOrderStyled = styled.section`
         grid-template-columns: 1fr 1fr 1fr 1fr;
         gap: 85px 60px;
         list-style: none;
-    }
-
-    .card {
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-        height: 330px;
-        width: 240px;
-        background: ${theme.colors.white};
-        padding: 50px 20px 10px 20px;
-        box-shadow: -8px 8px 20px 0px rgba(0, 0, 0, 0.20);
-        border-radius: ${theme.borderRadius.extraRound};
-
-        .card-img {
-            height: 145px;
-            width: 200px;
-            text-align: center;
-
-            img {
-                max-width: 100%;
-                max-height: 100%;
-            }
-        }
-
-        .card-title {
-            width: 100%;
-            overflow: hidden;
-            white-space: nowrap;
-            text-overflow: ellipsis;
-            margin: 15px 0 0 0;
-            color: #17161A;
-            font-size: 36px;
-            font-weight: 700;
-            text-align: start;
-        }
-
-        .card-description {
-            width: 100%;
-            display: grid;
-            grid-template-columns: 1fr 1fr;
-            align-items: center;
-            margin-top: 7.5px;
-
-            p {
-                margin: 0;
-                color: #FFA01B;
-                font-size: 16px;
-                font-weight: 400;
-                line-height: 22px;
-            }
-
-            .add_button-container {
-                width: 95px;
-
-                button {
-                    padding-top: ${theme.gridUnit * 1.5}px;
-                    padding-bottom: ${theme.gridUnit * 1.5}px;
-                }
-            }
-        }
     }
 `;
