d2b20923d36f57dac00e043a23714b2dca06eef4
Anhpati <romain-felicianne@hotmail.fr>
2023-12-04 00:34:36 +0100

    ♻️ F05: Replace Card description to CardDescription

--- src/components/ui/Card.jsx
+++ src/components/ui/Card.jsx
@@ -1,24 +1,16 @@
-import { PrimaryButton } from './PrimaryButton'
-import { formatPrice } from "../../utils/maths";
 import styled from 'styled-components';
 import { theme } from '../../theme';
 import { CardImg } from './CardImg';
 import { CardTitle } from './CardTitle';
 import { CardDescription } from './CardDescription';
+
 export const Card = ({ product }) => {
     return (
         <CardStyled key={product.id} className={'card'}>
             <CardImg src={product.imageSource} alt={product.title} />
             <CardTitle title={product.title} />
-            <CardDescription />
-            <div className={'card-description'}>
-                <p>
-                    {formatPrice(product.price)}
-                </p>
-                <div className={'add_button-container'}>
-                    <PrimaryButton label={'Ajouter'} />
-                </div>
-            </div>
+            <CardDescription price={product.price} />
+
         </CardStyled>
     )
 }
@@ -33,29 +25,4 @@ const CardStyled = styled.li`
     padding: 50px 20px 10px 20px;
     box-shadow: -8px 8px 20px 0px rgba(0, 0, 0, 0.20);
     border-radius: ${theme.borderRadius.extraRound};
-
-    .card-description {
-        width: 100%;
-        display: grid;
-        grid-template-columns: 1fr 1fr;
-        align-items: center;
-        margin-top: 7.5px;
-
-        p {
-            margin: 0;
-            color: #FFA01B;
-            font-size: 16px;
-            font-weight: 400;
-            line-height: 22px;
-        }
-
-        .add_button-container {
-            width: 95px;
-
-            button {
-                padding-top: ${theme.gridUnit * 1.5}px;
-                padding-bottom: ${theme.gridUnit * 1.5}px;
-            }
-        }
-    }
 `;
--- src/components/ui/CardDescription.jsx
+++ src/components/ui/CardDescription.jsx
@@ -1,5 +1,42 @@
-export const CardDescription = () => {
+import { PrimaryButton } from './PrimaryButton'
+import { formatPrice } from "../../utils/maths";
+import styled from 'styled-components';
+import { theme } from '../../theme';
+
+export const CardDescription = ({ price }) => {
     return (
-        <div>CardDescription</div>
+        <CardDescriptionStyled className={'card-description'}>
+            <p>
+                {formatPrice(price)}
+            </p>
+            <div className={'add_button-container'}>
+                <PrimaryButton label={'Ajouter'} />
+            </div>
+        </CardDescriptionStyled>
     )
 }
+
+const CardDescriptionStyled = styled.div`
+    width: 100%;
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    align-items: center;
+    margin-top: 7.5px;
+
+    p {
+        margin: 0;
+        color: #FFA01B;
+        font-size: 16px;
+        font-weight: 400;
+        line-height: 22px;
+    }
+
+    .add_button-container {
+        width: 95px;
+
+        button {
+            padding-top: ${theme.gridUnit * 1.5}px;
+            padding-bottom: ${theme.gridUnit * 1.5}px;
+        }
+    }
+`;
