1a93af3baf055e89316ebb6ca4f8aacc983100b6
Anhpati <romain-felicianne@hotmail.fr>
2023-12-03 22:16:48 +0100

    💄 ORDER > MainOrder layout

--- src/components/pages/order/MainOrder.jsx
+++ src/components/pages/order/MainOrder.jsx
@@ -4,12 +4,20 @@ import { theme } from "../../../theme";
 export const MainOrder = () => {
     return (
         <MainOrderStyled>
+            <section id="menu">
+                Menu
+            </section>
         </MainOrderStyled>
     )
 }
 
 const MainOrderStyled = styled.main`
     flex: 1;
+    display: flex;
     border-radius: 0 0 ${theme.borderRadius.extraRound} ${theme.borderRadius.extraRound};
     box-shadow: 0px 8px 20px 8px rgba(0, 0, 0, 0.20) inset;
+
+    #menu {
+        flex: 1;
+    }
 `;
