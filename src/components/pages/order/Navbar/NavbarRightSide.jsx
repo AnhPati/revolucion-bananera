63a0cb5647179b628a4e4f314ca49d678a658fad
Anhpati <romain-felicianne@hotmail.fr>
2023-12-04 18:28:15 +0100

    ✨ F06: NAVBAR > Add toast notification in NavbarRightSide

--- src/components/pages/order/Navbar/NavbarRightSide.jsx
+++ src/components/pages/order/Navbar/NavbarRightSide.jsx
@@ -1,18 +1,43 @@
+import { useState } from "react";
 import "react-toastify/dist/ReactToastify.css"
-import { theme } from "../../../../theme"
-import { ToastContainer } from "react-toastify"
-import styled from "styled-components"
+import { ToastContainer, toast } from "react-toastify"
 import { NavbarProfile } from "./NavbarProfile"
 import ToggleButton from "../../../ui/ToggleButton"
+import { IoMdInformationCircle } from "react-icons/io";
+import styled from "styled-components"
+import { theme } from "../../../../theme"
+
 
 export default function NavbarRightSide({ username }) {
+  const [adminMode, setAdminMode] = useState(false)
+
   const toggleLabelUnchecked = `Activer le mode admin`.toUpperCase()
   const toggleLabelChecked = `Désactiver le mode admin`.toUpperCase()
+
+  const notify = () => {
+    if (adminMode) {
+      return setAdminMode(!adminMode)
+    }
+
+    toast.info("Mode admin activé", {
+      icon: <IoMdInformationCircle size={30} />,
+      theme: "dark",
+      position: "bottom-right",
+      autoClose: 5000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+    })
+
+    setAdminMode(!adminMode)
+  }
 
   return (
     <NavbarRightSideStyled>
       <ToastContainer className="toaster" bodyClassName="body-toast" />
-      <ToggleButton labelIfChecked={toggleLabelChecked} labelIfUnchecked={toggleLabelUnchecked} />
+      <ToggleButton labelIfChecked={toggleLabelChecked} labelIfUnchecked={toggleLabelUnchecked} onToggle={notify} />
       <NavbarProfile username={username} />
     </NavbarRightSideStyled>
   )
@@ -35,8 +60,13 @@ const NavbarRightSideStyled = styled.div`
       margin-right: 20px;
       margin-left: 5px;
     }
+
     div {
       line-height: 1.3em;
+    }
+
+    svg {
+      color: #3498DB;
     }
   }
 `
