38c02c6719459d8d14ba350e2aff9804ce1933d9
Anhpati <romain-felicianne@hotmail.fr>
2023-12-15 17:54:47 +0100

    ✨ F07 : ORDER > Add navigation to AdminPanel, add body to AdminPanel

--- src/components/pages/order/MainOrder/AdminPanel.jsx
+++ src/components/pages/order/MainOrder/AdminPanel.jsx
@@ -1,9 +1,30 @@
 import styled from "styled-components";
 import { theme } from "../../../../theme";
+import { FiChevronUp } from "react-icons/fi";
+import { AiOutlinePlus } from "react-icons/ai";
+import { MdModeEditOutline } from "react-icons/md";
 
 const AdminPanel = () => {
     return (
-        <AdminPanelStyled>AdminPanel</AdminPanelStyled>
+        <AdminPanelStyled>
+            <nav className="admin_panel-nav">
+                <ul>
+                    <li>
+                        <FiChevronUp />
+                    </li>
+                    <li>
+                        <AiOutlinePlus /> Ajouter un produit
+                    </li>
+                    <li>
+                        <MdModeEditOutline /> Modifier un produit
+                    </li>
+                </ul>
+            </nav>
+            <div className="admin_panel-body">
+                Ajouter un produit
+            </div>
+
+        </AdminPanelStyled>
     )
 }
 
