1ca7b870ec6a115bc45f80a12164030e39b49fb2
Anhpati <romain-felicianne@hotmail.fr>
2023-12-15 19:32:16 +0100

    💄 F07 : ORDER > Add nav styles to AdminPanel

--- src/components/pages/order/MainOrder/AdminPanel.jsx
+++ src/components/pages/order/MainOrder/AdminPanel.jsx
@@ -1,6 +1,6 @@
 import styled from "styled-components";
 import { theme } from "../../../../theme";
-import { FiChevronUp } from "react-icons/fi";
+import { FiChevronUp, FiChevronDown } from "react-icons/fi";
 import { AiOutlinePlus } from "react-icons/ai";
 import { MdModeEditOutline } from "react-icons/md";
 
@@ -10,13 +10,21 @@ const AdminPanel = () => {
             <nav className="admin_panel-nav">
                 <ul>
                     <li>
-                        <FiChevronUp />
+                        <button>
+                            <FiChevronDown />
+                        </button>
                     </li>
                     <li>
-                        <AiOutlinePlus /> Ajouter un produit
+                        <button>
+                            <AiOutlinePlus />
+                            <span className="nav-text">Ajouter un produit</span>
+                        </button>
                     </li>
                     <li>
-                        <MdModeEditOutline /> Modifier un produit
+                        <button>
+                            <MdModeEditOutline />
+                            <span className="nav-text">Modifier un produit</span>
+                        </button>
                     </li>
                 </ul>
             </nav>
@@ -36,6 +44,57 @@ const AdminPanelStyled = styled.div`
     width: 1400px;
     border-radius: 0 0 0 15px;
     overflow: hidden;
+
+    .admin_panel-nav {
+        ul {
+            display: flex;
+            margin: 0;
+            padding-left: 71px;
+            list-style-type: none;
+
+            li {
+                button {
+                    height: 45px;
+                    padding: 10px 22px 11px 22px;
+                    background-color: ${theme.colors.white};
+                    font-family: 'Open Sans';
+                    color: #93A2B1;
+                    font-size: 14px;
+                    border: 1px solid #E4E5E9;
+                    border-radius: 5px 5px 0px 0px;
+                    cursor: pointer;
+
+                    svg {
+                        width: 16px;
+                        height: 16px;
+                    }
+
+                    .nav-text {
+                        margin-left: 10px;
+                        margin-right: 10px;
+                    }
+
+                    &:hover {
+                        .nav-text {
+                            border-bottom: #93A2B1 solid 2px;
+                        }
+
+                        &:active {
+                            .nav-text {
+                                border-bottom: ${theme.colors.white} solid 2px;
+                            }
+                        }       
+                    }
+
+                    &:active {
+                        background-color: #000;
+                        color: ${theme.colors.white};
+                        border: #000;
+                    }
+                }
+            }
+        }
+    }
 
     .admin_panel-body {
         width: 100%;
