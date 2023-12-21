c229e25e3cdebb3cf21950e370f2e78304a13516
Anhpati <romain-felicianne@hotmail.fr>
2023-12-21 19:09:30 +0100

    ✨ F08 : ADMIN > Add new product form in AdminPanelContent

--- src/components/pages/order/MainOrder/AdminPanel/AdminPanelContent.jsx
+++ src/components/pages/order/MainOrder/AdminPanel/AdminPanelContent.jsx
@@ -3,18 +3,57 @@ import { theme } from "../../../../../theme";
 import { useContext } from "react";
 import AdminContext from "../../../../../contexts/AdminContext";
 import { getAdminTabsConfig, getCurrentTabSelected } from "./helpers/getAdminTabsConfig";
+import { TextInput } from "../../../../ui/TextInput"
+import { PrimaryButton } from "../../../../ui/PrimaryButton";
+import { FaHamburger } from 'react-icons/fa'
+import { BsFillCameraFill } from 'react-icons/bs'
+import { MdOutlineEuro } from 'react-icons/md'
 
 const AdminPanelContent = () => {
     const { adminMode } = useContext(AdminContext)
     const isOpen = adminMode.adminPanel.isOpen
     const tabSelected = adminMode.adminPanel.tabSelected
 
+    const handleChange = (e) => {
+        setInputValue(e.target.value)
+    }
+
     const tabs = getAdminTabsConfig().slice(1)
     const currentTabSelected = getCurrentTabSelected(tabs, tabSelected)
-
+    const imgUrl = null
     return (
         <AdminPanelContentStyled className={isOpen ? '' : 'closed'}>
-            {currentTabSelected.label}
+            {/*currentTabSelected.label*/}
+            <form action="submit" >
+                <div>
+                    {imgUrl ? (
+                        <img src="" alt="" />
+                    ) : (
+                        <p>Aucune image</p>
+                    )}
+                </div>
+                <div>
+                    <TextInput
+                        value={''}
+                        onChange={handleChange}
+                        placeholder={'Nom du produit (ex: Super Burger)'}
+                        Icon={<FaHamburger />}
+                    />
+                    <TextInput
+                        value={''}
+                        onChange={handleChange}
+                        placeholder={`Lien URL d'une image (ex: https://la-photo-de-mon-produit.png)`}
+                        Icon={<BsFillCameraFill />}
+                    />
+                    <TextInput
+                        value={''}
+                        onChange={handleChange}
+                        placeholder={'Prix'}
+                        Icon={<MdOutlineEuro />}
+                    />
+                    <PrimaryButton />
+                </div>
+            </form>
         </AdminPanelContentStyled>
     )
 }
