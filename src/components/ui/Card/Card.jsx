858d0ddd75898e0d9db163f668bd0c076b7e2545
Anhpati <romain-felicianne@hotmail.fr>
2023-12-22 17:12:06 +0100

    ✨ F08 : ORDER > Add remove button in Card and add styles

--- src/components/ui/Card/Card.jsx
+++ src/components/ui/Card/Card.jsx
@@ -2,10 +2,14 @@ import styled from 'styled-components';
 import { theme } from '../../../theme';
 import { CardImg } from './CardImg';
 import { CardInfos } from './CardInfos';
+import { TiDelete } from 'react-icons/ti'
 
 export const Card = ({ id, imgSrc, title, leftDescription }) => {
     return (
         <CardStyled key={id} id={id} className={'card'}>
+            <span className='remove-button'>
+                <TiDelete />
+            </span>
             <CardImg src={imgSrc} alt={title} />
             <CardInfos title={title} leftDescription={leftDescription} />
         </CardStyled>
@@ -13,6 +17,7 @@ export const Card = ({ id, imgSrc, title, leftDescription }) => {
 }
 
 const CardStyled = styled.li`
+    position: relative;
     display: flex;
     flex-direction: column;
     align-items: center;
@@ -22,4 +27,20 @@ const CardStyled = styled.li`
     padding: 50px ${theme.spacing.md} 10px ${theme.spacing.md};
     box-shadow: ${theme.shadows.medium};
     border-radius: ${theme.borderRadius.extraRound};
+
+    .remove-button {
+        position: absolute;
+        right: 15px;
+        top: 15px;
+
+        svg {
+            font-size: 30px;
+            color: #FFA01B;
+            cursor: pointer;
+
+            :hover {
+                color: #E25549;
+            }
+        }
+    }
 `;
