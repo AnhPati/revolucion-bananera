be6a977ebe1d37b585547fc9006097ce596d10de
Anhpati <romain-felicianne@hotmail.fr>
2023-12-28 18:46:09 +0100

    ✨ F08 : ORDER > Create menu empty display in MenuOrder

--- src/components/pages/order/MainOrder/MenuOrder.jsx
+++ src/components/pages/order/MainOrder/MenuOrder.jsx
@@ -4,24 +4,36 @@ import { Card } from "../../../ui/Card/Card";
 import { formatPrice } from "../../../../utils/maths";
 import { theme } from "../../../../theme";
 import AdminContext from "../../../../contexts/AdminContext";
+import { PrimaryButton } from "../../../ui/PrimaryButton";
 
 export const MenuOrder = () => {
-    const { products } = useContext(AdminContext)
+    const { products, handleAddProduct } = useContext(AdminContext)
 
     return (
         <MenuOrderStyled>
-            <ul className="cards-list">
-                {products.map(({ id, imageSource, title, price }) => {
-                    return (
-                        <Card key={id}
-                            id={id}
-                            imgSrc={imageSource}
-                            title={title}
-                            leftDescription={formatPrice(price)}
-                        />
-                    )
-                })}
-            </ul>
+            {products.length > 0 ?
+                <ul className="cards-list">
+                    {products.map(({ id, imageSource, title, price }) => {
+                        return (
+                            <Card key={id}
+                                id={id}
+                                imgSrc={imageSource}
+                                title={title}
+                                leftDescription={formatPrice(price)}
+                            />
+                        )
+                    })}
+                </ul>
+                : <div className="menu-empty">
+                    <h1>
+                        Le menu est vide ?<br />
+                        Cliquez ci-dessous pour le réinitialiser
+                    </h1>
+                    <PrimaryButton
+                        label={'Générer de nouveaux produits'}
+                        onClick={handleAddProduct}
+                    />
+                </div>}
         </MenuOrderStyled>
     )
 }
@@ -39,5 +51,22 @@ const MenuOrderStyled = styled.section`
         padding-top: ${theme.gridUnit * 6.25}px;
         padding-bottom: ${theme.gridUnit * 6.25}px;
         list-style: none;
+    }
+
+    .menu-empty {
+        height: 100%;
+        width: 100%;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+
+        h1 {
+            text-align: center;
+        }
+
+        button {
+            width: 300px;
+        }
     }
 `;
