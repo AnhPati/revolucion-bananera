ae308115976edbfce4646878d82726c9864d342e
Anhpati <romain-felicianne@hotmail.fr>
2024-01-10 14:29:06 +0100

    ✨ F09 : ORDER > Add form with conditional rendering to UpdateProductForm

--- src/components/pages/order/MainOrder/AdminPanel/UpdateProductForm.jsx
+++ src/components/pages/order/MainOrder/AdminPanel/UpdateProductForm.jsx
@@ -2,14 +2,46 @@ import styled from "styled-components";
 import { HiCursorClick } from 'react-icons/hi'
 import AdminContext from "../../../../../contexts/AdminContext";
 import { useContext } from "react";
+import { ImgPreview } from "./ImgPreview";
+import { getTextInputsConfig } from "./helpers/getTextInputsConfig";
+import { TextInput } from "../../../../ui/TextInput";
+import { Button } from "../../../../ui/Button";
+import { theme } from "../../../../../theme";
 
 const UpdateProductForm = () => {
-    const { adminMode } = useContext(AdminContext)
+    const { adminMode, products } = useContext(AdminContext)
     const cardSelected = adminMode.adminPanel.cardSelected
+    const productSelected = products.find((product) => product.id === cardSelected)
+
+
+
+    const textInputs = cardSelected ? getTextInputsConfig(productSelected) : undefined
     return (
         <UpdateProductFormStyled>
-            {cardSelected ? (
-                <h2>{cardSelected}</h2>
+            {productSelected ? (
+                <form action='submit'>
+                    <ImgPreview
+                        src={productSelected.imageSource}
+                        alt={productSelected.title}
+                    />
+                    <div className='inputs-container'>
+                        {textInputs.map(textInput => {
+                            return (
+                                <TextInput
+                                    key={textInput.id}
+                                    {...textInput}
+                                    variant={'secondary'}
+                                />
+                            )
+                        })}
+                    </div>
+                    <div className='button-container'>
+                        <Button
+                            label={'Ajouter un nouveau produit au menu'}
+                            variant={'success'}
+                        />
+                    </div>
+                </form>
             ) : (
                 <h2>Cliquer sur un produit pour le modifier <HiCursorClick /></h2>
             )}
@@ -20,6 +52,28 @@ const UpdateProductForm = () => {
 export default UpdateProductForm
 
 const UpdateProductFormStyled = styled.div`
+    form {
+        display: grid;
+        grid-column-gap: ${theme.spacing.md};
+        grid-template-columns: repeat(6, 1fr);
+        grid-template-rows: repeat(5, 1fr);
+        height: 100%;
+        width: 100%;
+    }
+
+    .inputs-container {
+        grid-area: 1 / 2 / 4 / 5;
+        display: grid;
+        grid-template-rows: repeat(3, 1fr);
+    }
+
+    .button-container {
+        display: flex;
+        align-items: center;
+        grid-area: 4 / 2 / 5 / 5;
+        margin-top: 10px;
+    }
+
     h2 {
         margin-top: 49px;
         color: #747B91;
