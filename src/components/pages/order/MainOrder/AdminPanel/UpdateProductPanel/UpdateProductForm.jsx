9f974b56e1f684c014a78d45ca3bdb51cae43de2
Anhpati <romain-felicianne@hotmail.fr>
2024-01-11 20:15:43 +0100

    ♻️ F09 : ORDER > Extract update product form in UpdateProductPanel to UpdateProductForm and add UpdateProductForm to UpdateProductPanel

---
+++ src/components/pages/order/MainOrder/AdminPanel/UpdateProductPanel/UpdateProductForm.jsx
@@ -0,0 +1,72 @@
+import styled from "styled-components";
+import { Button } from "../../../../../ui/Button";
+import { TextInput } from "../../../../../ui/TextInput";
+import { theme } from "../../../../../../theme";
+import { ImgPreview } from "../ImgPreview";
+import { useContext, useState } from "react";
+import AdminContext from "../../../../../../contexts/AdminContext";
+import { getTextInputsConfig } from "../helpers/getTextInputsConfig";
+
+const UpdateProductForm = ({ productId }) => {
+    const { products } = useContext(AdminContext)
+    const productSelected = products.find((product) => product.id === productId)
+    const [productValues, setProductValues] = useState(productSelected)
+
+    const handleChange = (e) => {
+        const { name, value } = e.target
+        console.log(e.target)
+
+        setProductValues({ ...productValues, [name]: value })
+    }
+
+    const textInputs = getTextInputsConfig(productValues)
+    return (
+        <UpdateProductFormStyled action='submit'>
+            <ImgPreview
+                src={productSelected.imageSource}
+                alt={productSelected.title}
+            />
+            <div className='inputs-container'>
+                {textInputs.map(textInput => {
+                    return (
+                        <TextInput
+                            key={textInput.id}
+                            onChange={handleChange}
+                            {...textInput}
+                            variant={'secondary'}
+                        />
+                    )
+                })}
+            </div>
+            <div className='button-container'>
+                <Button
+                    label={'Ajouter un nouveau produit au menu'}
+                    variant={'success'}
+                />
+            </div>
+        </UpdateProductFormStyled>
+    )
+}
+
+export default UpdateProductForm
+const UpdateProductFormStyled = styled.form`
+    display: grid;
+    grid-column-gap: ${theme.spacing.md};
+    grid-template-columns: repeat(6, 1fr);
+    grid-template-rows: repeat(5, 1fr);
+    height: 100%;
+    width: 100%;
+
+    .inputs-container {
+        grid-area: 1 / 2 / 4 / 5;
+        display: grid;
+        grid-template-rows: repeat(3, 1fr);
+    }
+
+    .button-container {
+        display: flex;
+        align-items: center;
+        grid-area: 4 / 2 / 5 / 5;
+        margin-top: 10px;
+    }
+`;
--- src/components/pages/order/MainOrder/AdminPanel/UpdateProductPanel/UpdateProductPanel.jsx
+++ src/components/pages/order/MainOrder/AdminPanel/UpdateProductPanel/UpdateProductPanel.jsx
@@ -1,88 +1,28 @@
 import styled from "styled-components";
 import { HiCursorClick } from 'react-icons/hi'
 import AdminContext from "../../../../../../contexts/AdminContext";
-import { useContext, useState } from "react";
-import { ImgPreview } from "../ImgPreview";
-import { getTextInputsConfig } from "../helpers/getTextInputsConfig";
-import { TextInput } from "../../../../../ui/TextInput";
-import { Button } from "../../../../../ui/Button";
-import { theme } from "../../../../../../theme";
-import { EMPTY_PRODUCT } from "../../../../../../enums/product";
+import { useContext } from "react";
+import UpdateProductForm from "./UpdateProductForm";
 
 const UpdateProductPanel = () => {
-    const { adminMode, products } = useContext(AdminContext)
-    console.log
+    const { adminMode } = useContext(AdminContext)
     const cardSelected = adminMode.adminPanel.cardSelected
-    const productSelected = cardSelected ? products.find((product) => product.id === cardSelected) : EMPTY_PRODUCT
-    const [productValues, setProductValues] = useState(productSelected)
 
-    const handleChange = (e) => {
-        const { name, value } = e.target
-        console.log(e.target)
-
-        setProductValues({ ...productValues, [name]: value })
-    }
-
-    const textInputs = cardSelected && getTextInputsConfig(productValues)
     return (
         <UpdateProductPanelStyled>
             {cardSelected ? (
-                <form action='submit'>
-                    <ImgPreview
-                        src={productSelected.imageSource}
-                        alt={productSelected.title}
-                    />
-                    <div className='inputs-container'>
-                        {textInputs.map(textInput => {
-                            return (
-                                <TextInput
-                                    key={textInput.id}
-                                    onChange={handleChange}
-                                    {...textInput}
-                                    variant={'secondary'}
-                                />
-                            )
-                        })}
-                    </div>
-                    <div className='button-container'>
-                        <Button
-                            label={'Ajouter un nouveau produit au menu'}
-                            variant={'success'}
-                        />
-                    </div>
-                </form>
+                <UpdateProductForm productId={cardSelected} />
             ) : (
                 <h2>Cliquer sur un produit pour le modifier <HiCursorClick /></h2>
-            )}
-        </UpdateProductPanelStyled>
+            )
+            }
+        </UpdateProductPanelStyled >
     )
 }
 
 export default UpdateProductPanel
 
 const UpdateProductPanelStyled = styled.div`
-    form {
-        display: grid;
-        grid-column-gap: ${theme.spacing.md};
-        grid-template-columns: repeat(6, 1fr);
-        grid-template-rows: repeat(5, 1fr);
-        height: 100%;
-        width: 100%;
-    }
-
-    .inputs-container {
-        grid-area: 1 / 2 / 4 / 5;
-        display: grid;
-        grid-template-rows: repeat(3, 1fr);
-    }
-
-    .button-container {
-        display: flex;
-        align-items: center;
-        grid-area: 4 / 2 / 5 / 5;
-        margin-top: 10px;
-    }
-
     h2 {
         margin-top: 49px;
         color: #747B91;
