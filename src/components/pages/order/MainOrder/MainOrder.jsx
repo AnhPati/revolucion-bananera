c52435eb8f742b4aece381fc6c11af025531675c
Anhpati <romain-felicianne@hotmail.fr>
2024-01-16 18:53:02 +0100

    💄 F10 : ORDER > Add basket styles in MainOrder

--- src/components/pages/order/MainOrder/MainOrder.jsx
+++ src/components/pages/order/MainOrder/MainOrder.jsx
@@ -39,4 +39,59 @@ const MainOrderStyled = styled.main`
     border-radius: 0 0 ${theme.borderRadius.extraRound} ${theme.borderRadius.extraRound};
     box-shadow: ${theme.shadows.strong};
     overflow: hidden;
+
+    .basket-container {
+        display: flex;
+        flex-direction: column;
+        justify-content: space-between;
+        height: 100%;
+
+        .basket-header, .basket-footer {
+            height: 70px;
+            background-color: #000;
+        }
+
+        .basket-header {
+            display: flex;
+            align-items: center;
+            justify-content: space-between;
+            padding-left: 16px;
+            padding-right: 16px;
+
+            h3 {
+                margin: 0;
+                color: #FFA01B;
+                font-size: 36px;
+                font-weight: 400;
+                letter-spacing: 2px;
+            }
+        }
+
+        .basket-content {
+            flex: 1;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+
+            h2 {
+                margin: 0;
+                color: #747B91;
+                font-size: 36px;
+                font-weight: 400;
+                line-height: 72px; 
+            }
+        }
+
+        .basket-footer {
+            display: flex;
+            align-items: center;
+            justify-content: center;
+
+            h4 {
+                margin: 0;
+                color: #FFF;
+                font-size: 20px;
+            }
+        }
+    }
 `
