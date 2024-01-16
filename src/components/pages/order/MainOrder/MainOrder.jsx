d23d0334b759eaa13313c1e9993468b1e0e88363
Anhpati <romain-felicianne@hotmail.fr>
2024-01-16 18:37:40 +0100

    ✨ F10 : ORDER > Add basket content to MainOrder

--- src/components/pages/order/MainOrder/MainOrder.jsx
+++ src/components/pages/order/MainOrder/MainOrder.jsx
@@ -7,6 +7,26 @@ export const MainOrder = () => {
 
     return (
         <MainOrderStyled>
+            <div className={'basket-container'}>
+                <div className={'basket-header'}>
+                    <h3>
+                        Total
+                    </h3>
+                    <h3>
+                        0,00 €
+                    </h3>
+                </div>
+                <div className={'basket-content'}>
+                    <h2>
+                        Votre commande est vide.
+                    </h2>
+                </div>
+                <div className={'basket-footer'}>
+                    <h4>
+                        Codé avec ❤️ et React.JS
+                    </h4>
+                </div>
+            </div>
             <MainRightSide />
         </MainOrderStyled>
     )
