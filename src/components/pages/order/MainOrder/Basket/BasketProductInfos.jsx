d2b48296996ef5e384e01d62f2a3f93552539233
Anhpati <romain-felicianne@hotmail.fr>
2024-01-26 17:06:58 +0100

    ♻️ F11 : ORDER > Extract basket product quantity to BasketProductQuantity

--- src/components/pages/order/MainOrder/Basket/BasketProductInfos.jsx
+++ src/components/pages/order/MainOrder/Basket/BasketProductInfos.jsx
@@ -1,16 +1,13 @@
 import styled from "styled-components";
-import { MdDeleteForever } from "react-icons/md";
 import { theme } from "../../../../../theme";
 import { BasketProductLabel } from "./BasketProductLabel";
+import { BasketProductQuantity } from "./BasketProductQuantity";
 
 export const BasketProductInfos = ({ title, price, quantity, onClick }) => {
     return (
         <BasketProductInfosStyled>
             <BasketProductLabel title={title} price={price} />
-            <div className="basket-product_quantity" onClick={onClick}>
-                <span>x{quantity}</span>
-                <MdDeleteForever />
-            </div>
+            <BasketProductQuantity quantity={quantity} onClick={onClick} />
         </BasketProductInfosStyled>
     )
 }
@@ -20,19 +17,4 @@ const BasketProductInfosStyled = styled.div`
     grid-template-columns: 70% 1fr;
     margin-left: 21px;
     margin-right: ${theme.spacing.md};
-
-    .basket-product_quantity {
-        display: flex;
-        justify-content: flex-end;
-        align-items: center;
-
-        span {
-            color: ${theme.colors.primary};
-            font-size: ${theme.fonts.size.SM};
-        }
-
-        svg {
-            display: none;
-        }
-    }
 `;
---
+++ src/components/pages/order/MainOrder/Basket/BasketProductQuantity.jsx
@@ -0,0 +1,27 @@
+import { MdDeleteForever } from "react-icons/md";
+import styled from "styled-components";
+import { theme } from "../../../../../theme";
+
+export const BasketProductQuantity = ({ quantity, onClick }) => {
+    return (
+        <BasketProductQuantityStyled className="basket-product_quantity" onClick={onClick}>
+            <span>x{quantity}</span>
+            <MdDeleteForever />
+        </BasketProductQuantityStyled>
+    )
+}
+
+const BasketProductQuantityStyled = styled.div`
+    display: flex;
+    justify-content: flex-end;
+    align-items: center;
+
+    span {
+        color: ${theme.colors.primary};
+        font-size: ${theme.fonts.size.SM};
+    }
+
+    svg {
+        display: none;
+    }
+`;
