6795b713236f2bcd5233d0e0c9f07708bb173e8b
Anhpati <romain-felicianne@hotmail.fr>
2024-01-26 17:03:04 +0100

    ♻️ F11 : ORDER > Extract basket product label to BasketProductLabel

--- src/components/pages/order/MainOrder/Basket/BasketProductInfos.jsx
+++ src/components/pages/order/MainOrder/Basket/BasketProductInfos.jsx
@@ -1,15 +1,12 @@
 import styled from "styled-components";
-import { formatPrice } from "../../../../../utils/maths";
 import { MdDeleteForever } from "react-icons/md";
 import { theme } from "../../../../../theme";
+import { BasketProductLabel } from "./BasketProductLabel";
 
 export const BasketProductInfos = ({ title, price, quantity, onClick }) => {
     return (
         <BasketProductInfosStyled>
-            <div className="basket-product_left-infos">
-                <h5>{title}</h5>
-                <p>{formatPrice(price)}</p>
-            </div>
+            <BasketProductLabel title={title} price={price} />
             <div className="basket-product_quantity" onClick={onClick}>
                 <span>x{quantity}</span>
                 <MdDeleteForever />
@@ -23,28 +20,6 @@ const BasketProductInfosStyled = styled.div`
     grid-template-columns: 70% 1fr;
     margin-left: 21px;
     margin-right: ${theme.spacing.md};
-
-    .basket-product_left-infos {
-        display: grid;
-        grid-template-rows: 60% 40%;
-        padding: 5px 0 8px 0;
-        gap: 5px;
-
-        h5 {
-            margin: 0;
-            white-space: nowrap;
-            overflow: hidden;
-            text-overflow: ellipsis;
-            color: ${theme.colors.dark};
-            font-size: ${theme.fonts.size.P3};
-        }
-
-        p {
-            margin: 0;
-            color: ${theme.colors.primary};
-            font-size: ${theme.fonts.size.SM};
-        }
-    }
 
     .basket-product_quantity {
         display: flex;
---
+++ src/components/pages/order/MainOrder/Basket/BasketProductLabel.jsx
@@ -0,0 +1,34 @@
+import styled from "styled-components";
+import { formatPrice } from "../../../../../utils/maths";
+import { theme } from "../../../../../theme";
+
+export const BasketProductLabel = ({ title, price }) => {
+    return (
+        <BasketProductLabelStyled>
+            <h5>{title}</h5>
+            <p>{formatPrice(price)}</p>
+        </BasketProductLabelStyled>
+    )
+}
+
+const BasketProductLabelStyled = styled.div`
+    display: grid;
+    grid-template-rows: 60% 40%;
+    padding: 5px 0 8px 0;
+    gap: 5px;
+
+    h5 {
+        margin: 0;
+        white-space: nowrap;
+        overflow: hidden;
+        text-overflow: ellipsis;
+        color: ${theme.colors.dark};
+        font-size: ${theme.fonts.size.P3};
+    }
+
+    p {
+        margin: 0;
+        color: ${theme.colors.primary};
+        font-size: ${theme.fonts.size.SM};
+    }
+`;
