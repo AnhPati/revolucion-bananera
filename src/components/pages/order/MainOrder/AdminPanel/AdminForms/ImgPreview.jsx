eba45daf4ebeeec9c0868e37389f53d64bcfc6a7
Anhpati <romain-felicianne@hotmail.fr>
2024-02-03 20:34:38 +0100

    ✨ F14 : ORDER > Create ImgPreview image appearance animation

--- src/components/pages/order/MainOrder/AdminPanel/AdminForms/ImgPreview.jsx
+++ src/components/pages/order/MainOrder/AdminPanel/AdminForms/ImgPreview.jsx
@@ -1,15 +1,26 @@
 import styled, { css } from "styled-components";
 import { theme } from "../../../../../../theme";
+import { CSSTransition, TransitionGroup } from "react-transition-group";
 
 export const ImgPreview = ({ src, alt }) => {
     return (
         <ImgPreviewStyled $empty={src ? false : true}>
             {src ? (
-                <img src={src} alt={alt} />
+                <TransitionGroup component={null}>
+                    <CSSTransition
+                        key={alt}
+                        appear={true}
+                        classNames={'img-preview'}
+                        timeout={{ enter: 1000, exit: 0 }}
+                    >
+                        <img src={src} alt={alt} />
+                    </CSSTransition>
+                </TransitionGroup>
             ) : (
                 <p>Aucune image</p>
-            )}
-        </ImgPreviewStyled>
+            )
+            }
+        </ImgPreviewStyled >
     )
 }
 
@@ -22,6 +33,32 @@ const ImgPreviewStyled = styled.div`
     img {
         max-width: 100px;
         max-height: 100px;
+
+        &.img-preview-appear {
+            opacity: 0%;
+        }
+
+        &.img-preview-appear-active {
+            transition: 1s;
+            opacity: 100%;
+        }
+
+        &.img-preview-enter {
+            opacity: 0%;
+        }
+
+        &.img-preview-enter-active {
+            transition: 1s;
+            opacity: 100%;
+        }
+
+        &.img-preview-exit {
+            opacity: 100%;
+        }
+
+        &.img-preview-exit-active {
+            opacity: 0%;
+        }
     }
 
     p {
