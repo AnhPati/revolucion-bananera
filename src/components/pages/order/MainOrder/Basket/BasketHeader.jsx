37a3dace48cdebed10d886c6b3fed0e39e627050
Anhpati <romain-felicianne@hotmail.fr>
2024-02-03 19:03:20 +0100

    ✨ F14 : ORDER > Create Basket header casino effect to display amount

--- src/components/pages/order/MainOrder/Basket/BasketHeader.jsx
+++ src/components/pages/order/MainOrder/Basket/BasketHeader.jsx
@@ -4,10 +4,13 @@ import { theme } from "../../../../../theme";
 import { calculateAmountToPay } from "./helpers/calculateAmountToPay";
 import { useContext } from "react";
 import OrderContext from "../../../../../contexts/OrderContext";
+import { CSSTransition, TransitionGroup } from "react-transition-group";
 
 export const BasketHeader = () => {
     const { basketProducts, products } = useContext(OrderContext)
     const amountToPay = calculateAmountToPay(basketProducts, products)
+
+    const count = formatPrice(amountToPay)
 
     return (
         <BasketHeaderStyled>
@@ -15,7 +18,15 @@ export const BasketHeader = () => {
                 Total
             </h3>
             <h3>
-                {formatPrice(amountToPay)}
+                <TransitionGroup className={'casino-container'}>
+                    <CSSTransition
+                        key={count}
+                        classNames={'casino-count'}
+                        timeout={500}
+                    >
+                        <span>{count}</span>
+                    </CSSTransition>
+                </TransitionGroup>
             </h3>
         </BasketHeaderStyled>
     )
@@ -36,5 +47,39 @@ const BasketHeaderStyled = styled.div`
         font-size: ${theme.fonts.size.P4};
         font-weight: ${theme.fonts.weights.regular};
         letter-spacing: 2px;
+
+        .casino-container {
+            position: relative;
+            overflow-y: hidden;
+
+            span {
+                display: inline-block;
+
+                &.casino-count-enter {
+                    transform: translateY(50px);
+                    opacity: 0%;
+                }
+
+                &.casino-count-enter-active {    
+                        transform: translateY(0);
+                        transition: 0.3s;
+                        opacity: 100%;
+                }
+
+                &.casino-count-exit {
+                        position: absolute;
+                        bottom:0;
+                        right: 0;
+                        transform: translateY(0);     
+                        opacity: 100%;
+                }
+
+                &.casino-count-exit-active {  
+                    transform: translateY(-50px);
+                    opacity: 0%;  
+                    transition: 0.3s;
+                }
+            }
+        }
     }
 `;
