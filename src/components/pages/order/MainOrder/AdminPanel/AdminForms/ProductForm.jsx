9f895bfae35549ac6d9468db63fe63004ed69787
Anhpati <romain-felicianne@hotmail.fr>
2024-02-14 15:37:05 +0100

    ♻️ F15 : ORDER > Extract ProductForm input fields to ProductFormFields

--- src/components/pages/order/MainOrder/AdminPanel/AdminForms/ProductForm.jsx
+++ src/components/pages/order/MainOrder/AdminPanel/AdminForms/ProductForm.jsx
@@ -1,43 +1,23 @@
 import styled from "styled-components"
-import { TextInput } from "../../../../../ui/TextInput"
 import { ImgPreview } from "./ImgPreview"
-import { getSelectInputsConfig, getTextInputsConfig } from "../helpers/getInputsConfig"
 import { theme } from "../../../../../../theme"
 import { forwardRef } from "react"
-import { SelectInput } from "../../../../../ui/SelectInput"
+import ProductFormFields from "./ProductFormFields"
 
 export const ProductForm = forwardRef(({ product, onSubmit, onChange, children, onFocus, onBlur }, ref) => {
-    const textInputs = getTextInputsConfig(product)
-    const selectInputs = getSelectInputsConfig(product)
-
     return (
         <ProductFormStyled action='submit' onSubmit={onSubmit}>
             <ImgPreview
                 src={product.imageSource}
                 alt={product.title}
             />
-            <div className='inputs-container'>
-                {textInputs.map(textInput => (
-                    <TextInput
-                        key={textInput.id}
-                        {...textInput}
-                        onChange={onChange}
-                        onFocus={onFocus}
-                        onBlur={onBlur}
-                        variant={'secondary'}
-                        ref={ref && textInput.name === 'title' ? ref : null}
-                    />
-                ))}
-                {selectInputs.map(selectInput => (
-                    <SelectInput
-                        {...selectInput}
-                        key={selectInput.id}
-                        onChange={onChange}
-                        onFocus={onFocus}
-                        onBlur={onBlur}
-                    />
-                ))}
-            </div>
+            <ProductFormFields
+                product={product}
+                onChange={onChange}
+                onFocus={onFocus}
+                onBlur={onBlur}
+                ref={ref}
+            />
             <div className='form-footer'>
                 {children}
             </div>
@@ -53,26 +33,6 @@ const ProductFormStyled = styled.form`
     grid-template-rows: repeat(5, 1fr);
     height: 100%;
     width: 100%;
-
-    .inputs-container {
-        grid-area: 1 / 2 / 4 / 5;
-        display: grid;
-        grid-template-rows: repeat(3, 1fr);
-        grid-template-columns: repeat(3, 1fr);
-        gap: 8px;
-
-        .input-title {
-            grid-area: 1/1/2/4;
-        }
-        
-        .input-img {
-            grid-area: 2/1/3/4;
-        }
-
-        .input-price {
-            grid-area: 3/1/4/2;
-        }
-    }
 
     .form-footer {
         display: flex;
---
+++ src/components/pages/order/MainOrder/AdminPanel/AdminForms/ProductFormFields.jsx
@@ -0,0 +1,58 @@
+import { forwardRef } from "react"
+import styled from "styled-components"
+import { getSelectInputsConfig, getTextInputsConfig } from "../helpers/getInputsConfig"
+import { TextInput } from "../../../../../ui/TextInput"
+import { SelectInput } from "../../../../../ui/SelectInput"
+
+const ProductFormFields = forwardRef(({ product, onChange, onFocus, onBlur }, ref) => {
+    const textInputs = getTextInputsConfig(product)
+    const selectInputs = getSelectInputsConfig(product)
+
+    return (
+        <ProductFormFieldsStyled>
+            {textInputs.map(textInput => (
+                <TextInput
+                    key={textInput.id}
+                    {...textInput}
+                    onChange={onChange}
+                    onFocus={onFocus}
+                    onBlur={onBlur}
+                    variant={'secondary'}
+                    ref={ref && textInput.name === 'title' ? ref : null}
+                />
+            ))}
+            {selectInputs.map(selectInput => (
+                <SelectInput
+                    {...selectInput}
+                    key={selectInput.id}
+                    onChange={onChange}
+                    onFocus={onFocus}
+                    onBlur={onBlur}
+                />
+            ))}
+        </ProductFormFieldsStyled>
+    )
+})
+ProductFormFields.displayName = "ProductFormFields"
+
+export default ProductFormFields
+
+const ProductFormFieldsStyled = styled.div`
+    grid-area: 1 / 2 / 4 / 5;
+    display: grid;
+    grid-template-rows: repeat(3, 1fr);
+    grid-template-columns: repeat(3, 1fr);
+    gap: 8px;
+
+    .input-title {
+        grid-area: 1/1/2/4;
+    }
+    
+    .input-img {
+        grid-area: 2/1/3/4;
+    }
+
+    .input-price {
+        grid-area: 3/1/4/2;
+    }
+`;
