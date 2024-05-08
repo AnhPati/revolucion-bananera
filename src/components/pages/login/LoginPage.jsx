55c97cd3869432ae5c9e61c08dcd82b4921c76b0
Anhpati <romain-felicianne@hotmail.fr>
2024-05-08 21:23:03 +0200

    🎉 F14_b : LOGIN > Add splash screen animation.

--- src/components/pages/login/LoginPage.jsx
+++ src/components/pages/login/LoginPage.jsx
@@ -2,11 +2,20 @@ import styled from "styled-components";
 import { LoginForm } from "./LoginForm"
 import background from "./../../../assets/burger-background.jpg"
 import { Logo } from "../../ui/Logo";
+import { CSSTransition, TransitionGroup } from "react-transition-group";
+import { theme } from "../../../theme";
 
 const LoginPage = () => {
     return (
         <LoginPageStyled>
-            <Logo className={'login_page-logo'} />
+            <TransitionGroup>
+                <CSSTransition
+                    classNames={'screensplash-logo'}
+                    appear={true}
+                    timeout={500}>
+                    <Logo className={'login_page-logo'} />
+                </CSSTransition>
+            </TransitionGroup>
             <LoginForm />
         </LoginPageStyled>
     )
@@ -27,5 +36,54 @@ const LoginPageStyled = styled.div`
     .login_page-logo {
         transform: scale(2.5);
         margin: 40px 0;
+    }
+
+    .screensplash-logo-appear h1:first-child {
+        transform: translateX(-200px);
+        opacity: 0%;
+    }
+
+    .screensplash-logo-appear-active h1:first-child {
+        transform: translateX(0);
+        transition: ${theme.animations.speed.quick} ease-out;
+        opacity: 100%;
+    }
+
+    .screensplash-logo-appear h1:last-child {
+        transform: translateX(200px);
+        opacity: 0%;
+    }
+
+    .screensplash-logo-appear-active h1:last-child {
+        transform: translateX(0);
+        transition: ${theme.animations.speed.quick} ease-out;
+        opacity: 100%;
+    }
+
+    .screensplash-logo-appear .img-container {
+        transform: scale(2);
+        opacity: 0%;
+    }
+
+    .screensplash-logo-appear-active .img-container {
+        transform: translateX(0);
+        transition: ${theme.animations.speed.quick} ease-out;
+        opacity: 100%;
+    }
+
+    form {
+        animation: fadeInBottom ${theme.animations.speed.quick} ease-out;
+    }
+
+    @keyframes fadeInBottom {
+        0% {
+            transform: translateY(200px);
+            opacity: 0%;
+        }
+
+        100% {
+            transform: translateX(0%);
+            opacity: 100%;
+        }
     }
 `;
