aaed6912609e6800a0f2ca67e6d8f4b545218040
Anhpati <romain-felicianne@hotmail.fr>
2024-06-04 17:50:25 +0200

    💄 F18 : ADMIN > Add AdminOrdersPanel styles.

--- src/components/pages/order/MainOrder/AdminPanel/AdminOrdersPanel.jsx
+++ src/components/pages/order/MainOrder/AdminPanel/AdminOrdersPanel.jsx
@@ -3,19 +3,21 @@ import { Button } from "../../../../ui/Button"
 import { useContext } from "react"
 import OrderContext from "../../../../../contexts/OrderContext"
 import { RemoveButton } from "../../../../ui/Card/RemoveButton"
+import styled from "styled-components"
+import { theme } from "../../../../../theme"
 
 export const AdminOrdersPanel = () => {
     const { orders, products } = useContext(OrderContext)
     return (
-        <div>
-            <div>
+        <AdminOrdersPanelStyled>
+            <div className="orders-container">
                 {orders.map(order => {
                     return (
                         <div key={order.id}>
-                            <h6>N° de commande : {order.id}</h6>
-                            <h6>Nom : {order.userId}</h6>
-                            <h6>Heure de la commande : {order.orderTime.toString()}</h6>
-                            <h6>Commande :</h6>
+                            <p><b>N° de commande :</b> {order.id}</p>
+                            <p><b>Nom :</b> {order.userId}</p>
+                            <p><b>Heure de la commande :</b> {order.orderTime.toString()}</p>
+                            <p><b>Commande :</b></p>
                             <ul>
                                 {order.products.map(product => {
                                     const orderProduct = products[product.id]
@@ -40,6 +42,40 @@ export const AdminOrdersPanel = () => {
                     Icon={<MdOutlineHistory />}
                 />
             </div>
-        </div>
+        </AdminOrdersPanelStyled>
     )
 }
+
+const AdminOrdersPanelStyled = styled.div`
+    display: flex;
+    flex-direction: column;
+    gap: 10px;
+    padding-right: 50px;
+
+    .orders-container {
+        height: 150px;
+        padding: 10px 15px;
+        border: solid 1px ${theme.colors.greyLight};
+        border-radius: ${theme.borderRadius.round};
+        overflow: hidden;
+        overflow-y: scroll;
+        scrollbar-color: transparent transparent;
+
+        &:hover { 
+            scrollbar-color: initial;
+        }
+
+        p, li {
+            font-size: 12px;
+        }
+
+        p {
+            margin: 0;
+        }
+
+        ul {
+            margin: 0;
+            padding-left: 10px;
+        }
+    }
+`;
