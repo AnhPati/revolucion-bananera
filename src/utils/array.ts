fd676460a23c9d88acb147993c1cff37167c7973
Anhpati <romain-felicianne@hotmail.fr>
2024-12-05 13:08:21 +0100

    🚨 TS01 : UTILS > Fix array utils typescript parameters errors.

--- src/utils/array.ts
+++ src/utils/array.ts
@@ -1,9 +1,19 @@
-export const getDeepClone = (array) => JSON.parse(JSON.stringify(array))
+type ProductType = {
+    id: string,
+    title: string,
+    imageSource: string,
+    price: string,
+    quantity: number,
+    isAvailable: boolean,
+    isPublicised: boolean
+}
 
-export const findObjectById = (id, array) => array.find(item => item.id === id)
+export const getDeepClone = (array: Array<ProductType>) => JSON.parse(JSON.stringify(array))
 
-export const findIndexById = (id, array) => array.findIndex(item => item.id === id)
+export const findProductTypeById = (id: string, array: Array<ProductType>) => array.find(item => item.id === id)
 
-export const filterArrayWithId = (id, array) => array.filter(item => item.id !== id)
+export const findIndexById = (id: string, array: Array<ProductType>) => array.findIndex(item => item.id === id)
 
-export const isEmptyArray = (array) => array.length === 0
+export const filterArrayWithId = (id: string, array: Array<ProductType>) => array.filter(item => item.id !== id)
+
+export const isEmptyArray = (array: Array<ProductType>) => array.length === 0
