6b047b2b5c380c0f1794c0cd78d3612b13f8f5b2
Anhpati <romain-felicianne@hotmail.fr>
2024-12-06 15:38:25 +0100

    🐛 UTILS > Fix array functions name.

--- src/utils/array.ts
+++ src/utils/array.ts
@@ -8,9 +8,9 @@ type ProductType = {
     isPublicised: boolean
 }
 
-export const getDeepClone = (array: Array<ProductType>) => JSON.parse(JSON.stringify(array))
+export const getDeepClone = (array: Array<Object>) => JSON.parse(JSON.stringify(array))
 
-export const findProductTypeById = (id: string, array: Array<ProductType>) => array.find(item => item.id === id)
+export const findObjectById = (id: string, array: Array<ProductType>) => array.find(item => item.id === id)
 
 export const findIndexById = (id: string, array: Array<ProductType>) => array.findIndex(item => item.id === id)
 
