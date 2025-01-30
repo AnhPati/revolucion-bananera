2807f41d7dc983f7744ddb7f329cdb195bb42d81
Anhpati <romain-felicianne@hotmail.fr>
2025-01-30 23:24:20 +0100

    🐛 F19 : UTILS > Fix format date.

--- src/utils/date.js
+++ src/utils/date.js
@@ -1,10 +1,9 @@
 export const formatDate = (date) => {
-    const year = date.getFullYear()
-    const month = date.getMonth() + 1 < 10 ? `0${date.getMonth() + 1}` : date.getMonth() + 1
-    const day = date.getDay() < 10 ? `0${date.getDay()}` : date.getDay()
-    const hours = date.getHours() < 10 ? `0${date.getHours()}` : date.getHours()
-    const minutes = date.getMinutes() < 10 ? `0${date.getMinutes()}` : date.getMinutes()
-    const newDate = `${day}/${month}/${year} - ${hours}:${minutes}`
+    const year = date.getFullYear();
+    const month = date.getMonth() + 1 < 10 ? `0${date.getMonth() + 1}` : date.getMonth() + 1;
+    const day = date.getDate() < 10 ? `0${date.getDate()}` : date.getDate(); // 🔥 Correction ici
+    const hours = date.getHours() < 10 ? `0${date.getHours()}` : date.getHours();
+    const minutes = date.getMinutes() < 10 ? `0${date.getMinutes()}` : date.getMinutes();
 
-    return newDate
-}
+    return `${day}/${month}/${year} - ${hours}:${minutes}`;
+};
