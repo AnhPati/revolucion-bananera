56b5f936a15eac60ebfeaa466f5ab3ed4a70d842
Anhpati <romain-felicianne@hotmail.fr>
2025-09-10 01:43:55 +0200

    ♻️ TS03 : UTILS > Migrate orders utils function to TypeScript.

--- src/utils/orders.js
+++
@@ -1,3 +0,0 @@
-import { parseDate } from "./date"
-
-export const sortOrdersByDate = (orders) => [...orders].sort((a, b) => new Date(parseDate(b.orderTime)) - new Date(parseDate(a.orderTime)))
---
+++ src/utils/orders.ts
@@ -0,0 +1,11 @@
+import { parseDate } from "./date"
+
+export const sortOrdersByDate = <T extends { orderTime: string }>(orders: T[]): T[] | number => {
+    return [...orders].sort((a, b) => {
+        const firstOderTime = parseDate(a.orderTime)
+        const secondOderTime = parseDate(b.orderTime)
+
+        if (!firstOderTime || !secondOderTime) return 0
+        return secondOderTime.getTime() - firstOderTime.getTime()
+    })
+}
