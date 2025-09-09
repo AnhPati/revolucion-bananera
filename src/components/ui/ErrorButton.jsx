498b2f942268d8e46b68e06208a17562eb3a0945
Anhpati <romain-felicianne@hotmail.fr>
2025-09-09 21:16:00 +0200

    ⚰️ TS02 : UI > Remove useless ErrorButton component.

--- src/components/ui/ErrorButton.jsx
+++
@@ -1,9 +0,0 @@
-import { Link } from "react-router-dom"
-
-export const Button = ({ text, url }) => {
-    return (
-        <Link to={url}>
-            <button>{text}</button>
-        </Link>
-    )
-}
