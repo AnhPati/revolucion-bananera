100c1a567157bb646e29c6f792a7e5882698bc35
Anhpati <romain-felicianne@hotmail.fr>
2025-09-10 14:54:34 +0200

    ♻️ TS05 : UTILS > Replace ID union type by string type to array utils functions.

--- src/utils/array.ts
+++ src/utils/array.ts
@@ -1,11 +1,9 @@
 export const getDeepClone = <T>(array: T[]): T[] => JSON.parse(JSON.stringify(array))
 
-type ID = string | number
+export const findObjectById = <T extends { id: string }>(id: string, array: T[]): T | undefined => array.find(item => item.id === id)
 
-export const findObjectById = <T extends { id: ID }>(id: ID, array: T[]): T | undefined => array.find(item => item.id === id)
+export const findIndexById = <T extends { id: string }>(id: string, array: T[]): number => array.findIndex(item => item.id === id)
 
-export const findIndexById = <T extends { id: ID }>(id: ID, array: T[]): number => array.findIndex(item => item.id === id)
-
-export const filterArrayWithId = <T extends { id: ID }>(id: ID, array: T[]): T[] => array.filter(item => item.id !== id)
+export const filterArrayWithId = <T extends { id: string }>(id: string, array: T[]): T[] => array.filter(item => item.id !== id)
 
 export const isEmptyArray = (array: []): boolean => array.length === 0
