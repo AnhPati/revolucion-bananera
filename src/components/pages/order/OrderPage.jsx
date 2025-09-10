274a91c9118c97306c43de5caeac86bcf83b3727
Anhpati <romain-felicianne@hotmail.fr>
2025-09-10 17:44:29 +0200

    ♻️ TS07 : CONTEXT > Change OrderContext implementation to use order context provider and hooks. Create OrderPageContent to consume context data after context provide.

--- src/components/pages/order/OrderPage.jsx
+++ src/components/pages/order/OrderPage.jsx
@@ -1,118 +1,16 @@
 import styled from "styled-components";
-import Navbar from "./Navbar/Navbar"
 import { theme } from "../../../theme";
-import { MainOrder } from "./MainOrder/MainOrder";
-import OrderContext from "../../../contexts/OrderContext";
-import { useAdminProducts } from "../../../hooks/useAdminProducts";
-import { useBasketProducts } from "../../../hooks/useBasketProducts";
-import { useEffect } from "react";
-import { initialiseUserSession } from "./helpers/initialiseUserSession";
-import OrderConfirm from "./OrderConfirm";
-import { useAdminOrders } from "../../../hooks/useAdminOrders";
-import { getOrders } from "../../../api/orders"
-import { CSSTransition } from "react-transition-group";
+import { OrderContextProvider } from "../../../contexts/OrderContext";
+import { OrderPageContent } from "./OrderPageContent";
 import { OverlayMessageAnimation } from "../../../theme/animations";
 
 const OrderPage = () => {
-    const {
-        userId,
-        adminMode,
-        setAdminMode,
-        products,
-        setProducts,
-        handleAddProduct,
-        handleDeleteProduct,
-        handleGenerateNewProducts,
-        handleSelectProduct,
-        handleUpdateProduct,
-        productSelected,
-        titleInputRef
-    } = useAdminProducts()
-
-    const {
-        basketProducts,
-        setBasketProducts,
-        handleAddBasketProduct,
-        handleDeleteBasketProduct,
-        decrementQuantityProduct,
-        handleClearBasketProduct,
-    } = useBasketProducts()
-
-    const {
-        setOrders,
-        orderStatut,
-        orders,
-        tempOrder,
-        handleCheckOrder,
-        handleValidOrder,
-        handleDenyOrder,
-        handleArchiveOrder,
-        handleUnarchiveOrder,
-        handleDeleteOrder
-    } = useAdminOrders()
-
-
-    const orderContextValue = {
-        userId,
-        adminMode,
-        setAdminMode,
-        products,
-        handleAddProduct,
-        handleDeleteProduct,
-        handleGenerateNewProducts,
-        handleSelectProduct,
-        handleUpdateProduct,
-        productSelected,
-        titleInputRef,
-        basketProducts,
-        setBasketProducts,
-        handleAddBasketProduct,
-        handleDeleteBasketProduct,
-        decrementQuantityProduct,
-        handleClearBasketProduct,
-        setOrders,
-        orderStatut,
-        orders,
-        tempOrder,
-        handleCheckOrder,
-        handleValidOrder,
-        handleDenyOrder,
-        handleArchiveOrder,
-        handleUnarchiveOrder,
-        handleDeleteOrder
-    }
-
-    useEffect(() => {
-        initialiseUserSession(userId, adminMode.isAdminMode, setProducts, setBasketProducts, setOrders)
-    }, [])
-
-    useEffect(() => {
-        if (adminMode.isAdminMode && orders === undefined) {
-            const loadOrders = async () => {
-                const ordersData = await getOrders()
-                setOrders(ordersData)
-            }
-            loadOrders()
-        }
-    }, [adminMode.isAdminMode])
-
-
     return (
         <OrderPageStyled>
             <div className={'order-container'}>
-                <OrderContext.Provider value={orderContextValue}>
-                    <CSSTransition
-                        in={orderStatut === 'pending'}
-                        appear={true}
-                        classNames={'overlay-message-animation'}
-                        timeout={300}
-                        unmountOnExit
-                    >
-                        <OrderConfirm />
-                    </CSSTransition>
-                    <Navbar />
-                    <MainOrder />
-                </OrderContext.Provider>
+                <OrderContextProvider>
+                    <OrderPageContent />
+                </OrderContextProvider>
             </div>
         </OrderPageStyled>
     )
--- src/contexts/OrderContext.jsx
+++ src/contexts/OrderContext.jsx
@@ -1,4 +1,7 @@
-import { createContext } from "react";
+import { createContext, useContext } from "react";
+import { useAdminProducts } from "../hooks/useAdminProducts";
+import { useBasketProducts } from "../hooks/useBasketProducts";
+import { useAdminOrders } from "../hooks/useAdminOrders";
 
 const OrderContext = createContext({
     userId: '',
@@ -40,5 +43,52 @@ const OrderContext = createContext({
     handleDeleteOrder: () => { },
     handleClearBasketProduct: () => { }
 })
+
+export const OrderContextProvider = ({ children }) => {
+    const adminProductsData = useAdminProducts()
+    const basketProductsData = useBasketProducts()
+    const adminOrdersData = useAdminOrders()
+
+    const orderContextValue = {
+        userId: adminProductsData.userId,
+        adminMode: adminProductsData.adminMode,
+        setAdminMode: adminProductsData.setAdminMode,
+        products: adminProductsData.products,
+        setProducts: adminProductsData.setProducts,
+        handleAddProduct: adminProductsData.handleAddProduct,
+        handleDeleteProduct: adminProductsData.handleDeleteProduct,
+        handleGenerateNewProducts: adminProductsData.handleGenerateNewProducts,
+        handleSelectProduct: adminProductsData.handleSelectProduct,
+        handleUpdateProduct: adminProductsData.handleUpdateProduct,
+        productSelected: adminProductsData.productSelected,
+        titleInputRef: adminProductsData.titleInputRef,
+
+        basketProducts: basketProductsData.basketProducts,
+        setBasketProducts: basketProductsData.setBasketProducts,
+        handleAddBasketProduct: basketProductsData.handleAddBasketProduct,
+        handleDeleteBasketProduct: basketProductsData.handleDeleteBasketProduct,
+        decrementQuantityProduct: basketProductsData.decrementQuantityProduct,
+        handleClearBasketProduct: basketProductsData.handleClearBasketProduct,
+
+        orders: adminOrdersData.orders,
+        setOrders: adminOrdersData.setOrders,
+        orderStatut: adminOrdersData.orderStatut,
+        tempOrder: adminOrdersData.tempOrder,
+        handleCheckOrder: adminOrdersData.handleCheckOrder,
+        handleValidOrder: adminOrdersData.handleValidOrder,
+        handleDenyOrder: adminOrdersData.handleDenyOrder,
+        handleArchiveOrder: adminOrdersData.handleArchiveOrder,
+        handleUnarchiveOrder: adminOrdersData.handleUnarchiveOrder,
+        handleDeleteOrder: adminOrdersData.handleDeleteOrder
+    }
+
+    return (
+        <OrderContext.Provider value={orderContextValue}>
+            {children}
+        </OrderContext.Provider>
+    )
+}
+
+export const useOrderContext = () => useContext(OrderContext)
 
 export default OrderContext
