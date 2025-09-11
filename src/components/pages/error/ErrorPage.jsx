190c549b30f94775a0e5426ffb5eedd1fdebcfa6
Anhpati <romain-felicianne@hotmail.fr>
2025-09-11 23:40:35 +0200

    💄 TS08 : ERROR > Add ErrorPage styles.

--- src/components/pages/error/ErrorPage.jsx
+++ src/components/pages/error/ErrorPage.jsx
@@ -1,6 +1,8 @@
 import { Button } from "../../ui/Button"
 import { useNavigate } from "react-router-dom"
 import logo from "../../../assets/logo-orange.png"
+import styled from "styled-components"
+import { theme } from "../../../theme"
 
 const ErrorPage = () => {
     const navigate = useNavigate()
@@ -14,9 +16,9 @@ const ErrorPage = () => {
     }
 
     return (
-        <div>
-            <div>
-                <div>
+        <ErrorPageStyled>
+            <div className="error-content-container">
+                <div className="error-logo-container">
                     <h1>
                         4
                     </h1>
@@ -27,17 +29,68 @@ const ErrorPage = () => {
                         4
                     </h1>
                 </div>
-                <div>
+                <div className="error-text-container">
                     <h2>Il semblerait que tu tourne en rond après une frite...</h2>
                     <p>Je te propose de remettre les pieds dans le plat.</p>
                 </div>
             </div>
-            <div>
+            <div className="button-container">
                 <Button label={`Retour`} onClick={handleHomepageReturn} />
             </div>
 
-        </div>
+        </ErrorPageStyled>
     )
 }
 
 export default ErrorPage
+
+const ErrorPageStyled = styled.div`
+    height: 100vh;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-around;
+    align-items: center;
+    background: #f5f5f7;
+    
+    .error-content-container {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        
+        .error-logo-container {
+            display: flex;
+            align-items: center;
+            color: ${theme.colors.primary};
+        
+            h1 {
+                font-size: 200px;
+                font-weight: ${theme.fonts.weights.bold};
+                line-height: 1.5px;
+                margin: 0;
+            }
+        
+            .img-container {
+                width: 240px;
+                height: 180px;
+                margin: 0 ${theme.gridUnit * 0.625}px;
+        
+                img {
+                    width: 100%;
+                }
+            }
+        }
+
+        .error-text-container {
+            margin: 50px 0;
+            text-align: center;
+
+            h2 {
+                font-size: 30px;
+            }
+        }
+    }
+
+    .button-container {
+        width: 100px;
+    }
+`;
