88f56213550385148e2ec65ae1e83a6751b0c25f
Anhpati <romain-felicianne@hotmail.fr>
2025-09-13 21:45:35 +0200

    ♻️ TS13 : MENUORDER > Migrate EmptyMenu to TypeScript.

--- src/components/pages/order/MainOrder/MenuOrder/EmptyMenu.tsx
+++ src/components/pages/order/MainOrder/MenuOrder/EmptyMenu.tsx
@@ -1,7 +1,7 @@
 import styled from "styled-components";
-import { Button } from "../../../../ui/Button"
-import { useOrderContext } from "../../../../../contexts/OrderContext";
-import { theme } from "../../../../../theme";
+import { Button } from "@/components/ui/Button";
+import { useOrderContext } from "@/contexts/OrderContext";
+import { theme } from "@/theme/theme";
 
 const EmptyMenu = () => {
     const { adminMode, handleGenerateNewProducts, userId } = useOrderContext()
