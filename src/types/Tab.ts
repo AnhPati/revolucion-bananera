913e879c1f22b1c548335edc4b04bb1050dfa676
Anhpati <romain-felicianne@hotmail.fr>
2025-09-14 21:18:55 +0200

    🏷️ TS6 : TYPES > Add Tab type.

--- src/types/Tab.ts
+++ src/types/Tab.ts
@@ -1,1 +1,10 @@
+import { IconType } from "react-icons"
+
 export type TabValues = "tab-collapse" | "tab-add" | "tab-update" | "tab-orders"
+
+export type Tab = {
+    id: TabValues,
+    className: 'tab-control' | 'tab-control tab-active',
+    Icon: IconType,
+    onClick: React.MouseEventHandler<HTMLButtonElement>
+}
