56f8715bec6014c24c9847b755a3155ea0398690
Anhpati <romain-felicianne@hotmail.fr>
2025-09-15 17:26:40 +0200

    ⚰️ TS17 : THEME > Remove useless animations.js and index.ts files.

--- src/components/pages/order/MainOrder/Basket/EmptyBasket.tsx
+++ src/components/pages/order/MainOrder/Basket/EmptyBasket.tsx
@@ -1,5 +1,5 @@
 import styled from "styled-components"
-import { theme } from "@/theme"
+import { theme } from "@/theme/theme"
 import { BASKET_MESSAGE } from "@/constants/product"
 
 type EmptyBasketProps = {
