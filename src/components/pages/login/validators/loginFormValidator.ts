d0ce5e35c6398ad3208f187a728293ff1b02a1ec
Anhpati <romain-felicianne@hotmail.fr>
2025-09-22 21:01:50 +0200

    ✨ C01 : LOGIN > Create loginFormSchema.

--- src/components/pages/login/validators/loginFormValidator.ts
+++ src/components/pages/login/validators/loginFormValidator.ts
@@ -1,3 +1,5 @@
+import z from "zod"
+
 export const validateLoginForm = (username: string): string | null => {
     if (!username.trim()) return `Veuillez entrer un prénom.`
 
@@ -10,3 +12,12 @@ export const validateLoginForm = (username: string): string | null => {
 
     return null
 }
+
+export const loginFormSchema = z.object({
+    username: z
+        .string()
+        .nonempty(`Veuillez entrer un prénom.`)
+        .min(2, `Le prénom doit contenir au moins 2 caractères.`)
+        .max(20, `Le prénom ne peut pas dépasser 20 caractères.`)
+        .regex(/^[\p{L}_-]+$/u, `Le prénom ne doit contenir que des lettres ou '-', pas d'autres caractères spéciaux.`)
+})
