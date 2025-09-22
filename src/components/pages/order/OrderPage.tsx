080085f3ae0ad070f7cffa655eece51b5ec12319
Anhpati <romain-felicianne@hotmail.fr>
2025-09-22 15:43:20 +0200

    💄 TS24 : ORDER > Add admin shortcuts styles.

--- src/components/pages/order/OrderPage.tsx
+++ src/components/pages/order/OrderPage.tsx
@@ -53,5 +53,40 @@ const OrderPageStyled = styled.div`
         position: absolute;
         top: 40px;
         left: 40px;
+        z-index: 1;
+        display: flex;
+        flex-direction: column;
+        gap: ${theme.spacing.sm};
+
+        padding: 20px;
+        background: ${theme.colors.dark};
+        color: ${theme.colors.white};
+        border-radius: ${theme.borderRadius.round};
+
+        p {
+            margin: 0;
+
+            &.shortcuts-title {
+                font-size: ${theme.fonts.size.P0};
+                font-weight: ${theme.fonts.weights.bold};
+            }
+
+            &.shortcuts-info {
+                font-size: ${theme.fonts.size.SM};
+                font-weight: ${theme.fonts.weights.regular};
+            }
+        }
+
+        button {
+            width: 220px;
+            padding: 13px 0;
+            font-size: 13.5px;
+            font-weight: ${theme.fonts.weights.regular};
+            background: ${theme.colors.dark};
+            color: ${theme.colors.white};
+            border: 1px solid ${theme.colors.white};
+            border-radius: 25px;
+        }
+        
     }
 `;
