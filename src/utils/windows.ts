ccc3e9b8a3409e11fc0a09cecb75d23a18a319f2
Anhpati <romain-felicianne@hotmail.fr>
2025-09-22 17:14:31 +0200

    ♻️ TS24 : UTILS > Extract local storage logics basketProduct to window utils and use in basketProduct with type guard.

--- src/utils/windows.ts
+++ src/utils/windows.ts
@@ -1,3 +1,12 @@
 export const windowRefresh = () => {
     window.location.reload()
 }
+
+export const setLocalStorage = <T>(key: string, data: T) => {
+    localStorage.setItem(key, JSON.stringify(data))
+}
+
+export const getLocalStorage = (key: string): unknown | null => {
+    const item = localStorage.getItem(key)
+    return item ? JSON.parse(item) : null
+}
